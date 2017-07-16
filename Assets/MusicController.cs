72d083c3b92708908432743477e4a6f442914d7f
danisoares <danielespassos@gmail.com>
2017-07-16 12:13:55 -0300

    audios completos

---
+++ Assets/MusicController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicController : MonoBehaviour {
+    public AudioMixer mixer;
+    public AudioMixerSnapshot[] snaps;
+	// Use this for initialization
+	void Start () {
+        //snaps = new AudioMixerSnapshot[];
+        //snaps = mixer.FindSnapshot("");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //mixer.TransitionToSnapshots(snaps, new float[] { 0.1f }, 0.1f);
+	}
+}
---
+++ Assets/Scripts/Trigger/PositionTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+		if (collision.gameObject.CompareTag("Obstacle")
+           || collision.gameObject.CompareTag("Sheep")
+           || collision.gameObject.CompareTag("Enemy"))
+		{
+			float xposition = 0;
+
+			if (collision.transform.position.x > 0)
+				xposition = 2;
+			else if (collision.transform.position.x < 0)
+                xposition = -2;
+
+            collision.transform.position = new Vector3(xposition, collision.transform.position.y, 0);
+		}
+    }
+}
